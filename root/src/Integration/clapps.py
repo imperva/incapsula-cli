9e201eb54610f8a955cf9c874c6a30f7737c02ce
Joe Moore <joe.moore@imperva.com>
2019-07-30 14:43:06 -0700

    Updated the clapps module to check for the clapps.json modified date and update if older than a day.

--- root/src/Integration/clapps.py
+++ root/src/Integration/clapps.py
@@ -1,11 +1,14 @@
 import json
 import os
+import time
+
 from Utils.executeRest import execute
 import logging
 from Utils.incapError import IncapError
 
 
 def get_clapps(cl_id):
+    filename = os.path.expanduser('~') + '/.incap/exports/clapps.json'
     cl_id = cl_id[0]
     logging.debug('Retrieving client application name and application type for Client ID:{}.'.format(cl_id))
     param = {
@@ -14,18 +17,22 @@ def get_clapps(cl_id):
     }
 
     try:
-        filename = os.path.expanduser('~') + '/.incap/exports/clapps.json'
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         if not os.path.isfile(filename):
             return get_update_file(filename, param, cl_id)
         with open(filename, 'r') as f:
-            result = json.load(f)
+            if (time.time() - os.path.getmtime(filename)) > 86400:
+                print("Updating the local exported clapps.json")
+                return get_update_file(filename, param, cl_id)
+            else:
+                result = json.load(f)
         if cl_id in result['clientAppTypes']:
             return result['clientAppTypes'][cl_id] + ' with client name ' + result['clientApps'][cl_id]
         else:
             return get_update_file(filename, param, cl_id)
     except OSError as e:
         logging.error(e.strerror)
+        return get_update_file(filename, param, cl_id)
 
 
 def get_update_file(filename, param, cl_id):
